79b453f0ace672cca2c65485610b3b553caee49d
rraefelix@gmail.com <rraefelix@gmail.com>
2026-05-25 17:11:43 +0800

    fix scrollToTop and some text

--- src/components/layout/ScrollToTop.tsx
+++ src/components/layout/ScrollToTop.tsx
@@ -2,11 +2,20 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export function ScrollToTop() {
-  const { pathname } = useLocation()
+  const { hash, pathname } = useLocation()
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
-  }, [pathname])
+    if (!hash) {
+      window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+      return
+    }
+
+    const frame = window.requestAnimationFrame(() => {
+      document.getElementById(hash.slice(1))?.scrollIntoView()
+    })
+
+    return () => window.cancelAnimationFrame(frame)
+  }, [hash, pathname])
 
   return null
 }
